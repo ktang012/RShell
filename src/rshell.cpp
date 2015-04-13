88a6bb912df7a9289154ebb44483e4fd06c03b4f
Kevin Tang <ktang012@ucr.edu>
2015-04-13 02:22:32 -0700

    implemented parsing method

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,34 +1,80 @@
-#include <iostream>
-#include <unistd.h>
-#include <vector>
 #include <queue>
+#include <cstring>
+#include <iostream>
+#include <vector>
 #include <string.h>
+#include <boost/tokenizer.hpp>
+
 using namespace std;
+using namespace boost;
 
-void parse_cmd_arg(string &input, queue<string> q) {
+// Cases to test:
+// cd Desktop&&ls -a
+// cd Desktop&[ENTER] ls -a
+// cd Desktop;ls -a
+// cd Desktop;ls
+// ls;ls;ls;ls
+// cd Desktop & ls -a
+// cd Desktop;
+// ls;
+// cd Desktop||ls -a;vim touch.cpp&&[ENTER] ls -a
+// cd Desktop
 
+void pcmd_prompt() {
+    // REMINDER: add user to prompt message
+    cout << "$ ";
+}
+
+void parse_into_queue(queue<string> &l, const string &s) {
+    char_separator<char> delim(" ", ";&|#") ;
+    tokenizer<char_separator<char>> mytok(s, delim);
+
+    for (auto i = mytok.begin(); i != mytok.end(); ++i) {
+        l.push(*i);
+    }
+}
+
+void init_cmd_queues(queue<string> &arg, queue<string> &con, queue<string> &l) {
+     while (!l.empty()) {
+         while (l.front() == "&" || l.front() == "|" || l.front() == ";") {
+              con.push(l.front());
+              l.pop();
+         }
+         arg.push(l.front());
+         l.pop();
+    }
+}
+
+void print_queue (queue<string> q) {
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
 
 int main(int argc, char* argv[]) {
+    // needs to be in while loop -- loop until "exit"
     string cmd_input;
-    queue<string> parsed_input;
-    while (1) {
-        // Get input
-        // Parse input
-            // 1) Add spaces between every connector - ?
-            // 2) Use tokenizing methods to get substrings
-            // 3) Add substrings onto queue
-        // While queue is NOT empty
-            // 1) Pop first item
-                // Check if "exit"
-            // 2) Continue popping flags UNTIL connector - stop
-            // 3) Execute command
-            // 4) Pop connector
-        cin >> cmd_input;
-        parse_cmd_arg(cmd_input);
+    queue<string> list;
+    pcmd_prompt();
+    getline(cin, cmd_input);
 
-        while (!parsed_input.empty()) {
+    parse_into_queue(list, cmd_input);
 
+    print_queue(list);
 
-    }
+    // while list is NOT empty
+        // get executable -- check if "exit"
+            // if "exit" -- exit the ENTIRE program!!
+            // else place into exec cstring
+        // if valid connector -- "&&", "||", ";"
+            // begin execution of exec and flags
+            // return any errors
+            // check logic to see if we continue through list -- break if needed
+        // else if flag
+            // place into flags cstrings
+        // continue through list
+
     return 0;
 }
