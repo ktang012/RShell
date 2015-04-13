0f2f8d1952c94c551c0a97db02e3067b645b0605
Kevin Tang <ktang012@ucr.edu>
2015-04-13 01:11:36 -0700

    Outlined rshell.cpp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <unistd.h>
 #include <vector>
-
+#include <queue>
+#include <string.h>
 using namespace std;
+
+void parse_cmd_arg(string &input, queue<string> q) {
+
 
 int main(int argc, char* argv[]) {
     string cmd_input;
+    queue<string> parsed_input;
     while (1) {
-        cout << "$ ";
+        // Get input
+        // Parse input
+            // 1) Add spaces between every connector - ?
+            // 2) Use tokenizing methods to get substrings
+            // 3) Add substrings onto queue
+        // While queue is NOT empty
+            // 1) Pop first item
+                // Check if "exit"
+            // 2) Continue popping flags UNTIL connector - stop
+            // 3) Execute command
+            // 4) Pop connector
         cin >> cmd_input;
-        // Parse
-            // check for errors while parsing
-                // [always exec] [maybe flag] [maybe connector]
-                // parse input
-                    // 1) first is exec
-                    // 2) maybe everything after is a flag
-                    // 3) until connector
-            // add spaces in between connectors
-            // parse using strtok
-            //
+        parse_cmd_arg(cmd_input);
+
+        while (!parsed_input.empty()) {
 
 
-
-
-        // break out of loop if "exit"
     }
     return 0;
 }
