f6a99abcf7b3afbd012690504aab0e756f5cc523
Kevin Tang <ktang012@ucr.edu>
2015-04-13 14:50:52 -0700

    made rshell.cpp skeleton

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -27,19 +27,20 @@ using namespace boost;
 // NOTE: cd will NOT be tested or used!
 // Re-read specs if unsure!!
 
+// prints user and machine info
 void pcmd_prompt() {
-    string login(getlogin());
-    struct utsname hostname;
-    uname(&hostname);
-    string sysName(hostname.sysname);
-    string machineName(hostname.machine);
-    string nodeName(hostname.nodename);
-    cout << login << "@" << nodeName << "$ ";
     // <sys/utsname.h> defines the struct utsname which includes info such as:
     // sysname, nodename, release, version, and machine
     // uname returns the struct with the wanted information
+
+    string login(getlogin());
+    struct utsname hostname;
+    uname(&hostname);
+    string nodeName(hostname.nodename);
+    cout << login << "@" << nodeName << "$ ";
 }
 
+// parses input into queue for execution
 void parse_into_queue(queue<string> &l, const string &s) {
     char_separator<char> delim(" ", ";&|#") ;
     tokenizer<char_separator<char>> mytok(s, delim);
@@ -49,20 +50,13 @@ void parse_into_queue(queue<string> &l, const string &s) {
     }
 }
 
-void init_cmd_queues(queue<string> &arg, queue<string> &con, queue<string> &l) {
-     while (!l.empty()) {
-         while (l.front() == "&" || l.front() == "|" || l.front() == ";") {
-              con.push(l.front());
-              l.pop();
-         }
-         arg.push(l.front());
-         l.pop();
-    }
-}
-
+// for testing purposes
 void print_queue (queue<string> q) {
+    if (q.empty()) {
+        return;
+    }
     while (!q.empty()) {
-        cout << q.front() << " ";
+        cout << "[" << q.front() << "] ";
         q.pop();
     }
     cout << endl;
@@ -78,6 +72,88 @@ int main(int argc, char* argv[]) {
     parse_into_queue(list, cmd_input);
 
     print_queue(list);
+
+    cout << "init size: " << list.size() << endl;
+
+    vector<string> cmd;
+    while (!list.empty()) {
+       while (!list.empty() && list.front() != "&" && list.front() != "|" && list.front() != ";") {
+         cmd.push_back(list.front());
+         list.pop();
+       }
+
+       if (cmd.size() == 0) {
+           cout << "syntax error!" << endl;
+           return 1;
+       }
+       else if (cmd.at(0) == "exit") {
+         cout << "Exiting RShell" << endl;
+         return 0;
+       }
+       else {
+         // begin exec
+         // return error flag
+         // check logic to see if we continue
+         // NOTE: cmd.at(0) = executable, everything else is flag
+         if (cmd.size() == 1) {
+             cout << "performing executable [" <<  cmd.at(0) << "] with NO flags";
+         }
+         else {
+            cout << "performing executable [" <<  cmd.at(0) << "] with flags: ";
+            for (int i = 1; i < cmd.size(); ++i) {
+            cout << cmd.at(i) << " ";
+            }
+         }
+         cout << endl;
+       }
+
+       cout << "queue size after processing cmd: " << list.size() << endl;
+       print_queue(list);
+
+       if (!list.empty() && list.front() == "&") {
+         list.pop();
+         if (list.front() == "&") {
+             // check logic
+             list.pop();
+         }
+         else {
+             cout << "&" << list.front() << " is not a connector!" << endl;
+             return 1;
+         }
+         cout << "queue size after processing connector: " << list.size() << endl;
+         print_queue(list);
+       }
+       else if (!list.empty() && list.front() == "|") {
+         list.pop();
+         if (list.front() == "|") {
+             // check logic
+             list.pop();
+          }
+          else {
+             cout << "|" << list.front() << " is not a connector!" << endl;
+             return 1;
+          }
+          cout << "queue size after processing connector: " << list.size() << endl;
+          print_queue(list);
+       }
+       else if (!list.empty() && list.front() == ";") {
+         // go onto next
+         list.pop();
+         cout << "queue size after processing connector: " << list.size() << endl;
+         print_queue(list);
+       }
+       else if (!list.empty())  {
+         cout << list.front() <<  " is not a connector!" << endl;
+         return 1;
+       }
+       else {
+         cout << "queue size after processing connector: " << list.size() << endl;
+         print_queue(list);
+       }
+       cmd.clear();
+    }
+
+
 
     // while list is NOT empty
         // get executable -- check if "exit"
